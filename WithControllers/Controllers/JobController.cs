aaf80c63259a85a517368671b682d4633320fd4c
CasperSambleben <Samblebenc@gmail.com>
2022-03-15 11:09:20 +0100

    Controllers added, job converted WIP

---
+++ WithControllers/Controllers/JobController.cs
@@ -0,0 +1,110 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MM.Data;
+using MM.Models;
+
+namespace MM.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobController : ControllerBase
+    {
+        private readonly MMDb _context;
+
+        public JobController(MMDb context)
+        {
+            _context = context;
+        }
+
+        // GET: api/JobController
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Job>>> GetJob()
+        {
+            return await _context.Jobs.ToListAsync();
+        }
+
+        // GET: api/Todoes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Job>> GetTodo(int id)
+        {
+            var todo = await _context.Jobs.FindAsync(id);
+
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            return todo;
+        }
+
+        // PUT: api/Todoes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutJob(int id, Job job)
+        {
+            if (id != job.JobId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(job).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!JobExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Todoes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Job>> PostTodo(Job job)
+        {
+            _context.Jobs.Add(job);
+            await _context.SaveChangesAsync();
+
+            //return CreatedAtAction("GetTodo", new { id = todo.Id }, todo);
+            return CreatedAtAction(nameof(GetJob), new { id = job.JobId }, job);
+        }
+
+        // DELETE: api/Todoes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteJob(int id)
+        {
+            var job = await _context.Jobs.FindAsync(id);
+            if (job == null)
+            {
+                return NotFound();
+            }
+
+            _context.Jobs.Remove(job);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool JobExists(int id)
+        {
+            return _context.Jobs.Any(e => e.JobId == id);
+        }
+    }
+}
