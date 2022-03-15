42bd59e24b132d75ab19c2b1899ccb5d534a1a04
CasperSambleben <Samblebenc@gmail.com>
2022-03-15 10:43:44 +0100

    Models added

---
+++ WithControllers/Models/Expense.cs
@@ -0,0 +1,14 @@
+﻿namespace MM.Models
+{
+    public class Expense
+    {
+        public long ExpenseId { get; set; }
+        public long ModelId { get; set; }
+        public long JobId { get; set; }
+        //[Column(TypeName = "date")]
+        public DateTime Date { get; set; }
+        public string? Text { get; set; }
+        //[Column(TypeName = "decimal(9,2)")]
+        public decimal amount { get; set; }
+    }
+}
